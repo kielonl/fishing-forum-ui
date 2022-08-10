1b3c4366aea344cf951c9444681b0112ea494587
kielon <mosadowski23@gmail.com>
2022-08-10 14:03:50 +0200

    added front page

---
+++ src/mainPage/components/navBar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "../styles/navBar.css";
+
+const NavBar = () => {
+  return (
+    <>
+      <ul className="navbar-main">
+        <li className="navbar-item">wedkarstwo</li>
+        <li className="navbar-item">rybactwo</li>
+        <li className="navbar-item">myslictwo</li>
+        <li className="navbar-item">kulinarne</li>
+        <li className="navbar-item navbar-login">
+          <button className="navbar-login-button">Log in</button>
+        </li>
+      </ul>
+    </>
+  );
+};
+
+export default NavBar;
---
+++ src/mainPage/mainPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "./components/navBar";
+import ImageBanner from "./components/imageBanner";
+import Content from "./components/Content";
+const MainPage = () => {
+  return (
+    <>
+      <NavBar />
+      <ImageBanner />
+      <Content />
+    </>
+  );
+};
+export default MainPage;
