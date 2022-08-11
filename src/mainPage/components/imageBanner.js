d1c89ebf4cb7c1d97bd077c3cc2d3ef49bb961a4
kielon <mosadowski23@gmail.com>
2022-08-11 14:41:58 +0200

    added image slider

--- src/mainPage/components/imageBanner.js
+++ src/mainPage/components/imageBanner.js
@@ -1,16 +1,44 @@
 import React from "react";
-import "../styles/ImageBanner.css";
+import { Slide } from "react-slideshow-image";
+import "../styles/imageBanner.css";
 
 const ImageBanner = () => {
+  const slideImages = [
+    {
+      url: "https://ipla.pluscdn.pl/dituel/cp/1t/1tk62pu34rqvkhg3c26zxrpq3d7xgfdb.jpg",
+      caption: "Slide 1",
+    },
+    {
+      url: "https://imgcdn1.przelom.pl/im/v1/news-900-widen-wm/2020/03/30/30423_1585584202_97964700.jpg",
+      caption: "Slide 2",
+    },
+    {
+      url: "https://d-art.ppstatic.pl/kadry/k/r/1/93/33/58c04527eddcc_o_medium.jpg",
+      caption: "Slide 3",
+    },
+  ];
+  const properties = {
+    duration: 2000,
+    autoplay: true,
+    transitonDuration: 500,
+    arrows: true,
+    infinite: true,
+    indicators: true,
+  };
   return (
     <div className="imageBanner-container">
       <div className="imageBanner-text-banner">
         <div>WEDKARZE TYGODNIA</div>
       </div>
-      <img
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuo_VfGXmQbbJxy4aRPGOh-uyQsPycHq_HEQ&usqp=CAU"
-        alt="fisherman"
-      />
+      <Slide className="image-slider" {...properties}>
+        {slideImages.map((slideImage, index) => (
+          <div className="each-slide" key={index}>
+            <div style={{ backgroundImage: `url(${slideImage.url})` }}>
+              {/* <span>{slideImage.caption}</span> */}
+            </div>
+          </div>
+        ))}
+      </Slide>
     </div>
   );
 };
