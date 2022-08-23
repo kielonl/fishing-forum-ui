4d88482d003baefdf38675d3edb04f59800d308a
kielon <mosadowski23@gmail.com>
2022-08-23 14:09:17 +0200

    rewrote api call

--- src/content/components/ListPosts.js
+++ src/content/components/ListPosts.js
@@ -4,18 +4,17 @@ import { PostContext } from "../../contexts/postContext";
 
 const ListPosts = () => {
   const post = useContext(PostContext);
-  const listPosts = post?.result?.map(({ title, content, image, post_id }) => {
-    // post.reactions.map((reaction, index) => {
-    //   console.log(reaction);
-    // });
-    return (
-      <div key={post_id} className="content-post">
-        <h1 className="content-post-title">{title}</h1>
-        <div className="content-post-description">{content}</div>
-        <img src={image} alt="" className="message-image" />
-      </div>
-    );
-  });
+  const listPosts = post?.result?.map(
+    ({ title, content, image, post_id, likes }) => {
+      return (
+        <div key={post_id} className="content-post">
+          <h1 className="content-post-title">{title}</h1>
+          <div className="content-post-description">{content}</div>
+          <img src={image} alt="" className="message-image" />
+        </div>
+      );
+    }
+  );
   return <div>{listPosts}</div>;
 };
 
--- src/content/components/LoadPosts.js
+++ src/content/components/LoadPosts.js
@@ -1,20 +1,26 @@
 import React, { useContext, useEffect } from "react";
 
 import ListPosts from "./ListPosts";
-import { useApiCall } from "../../api/useApiCall";
+import { PullData } from "../../api/useApiCall";
 
 import { PostContextUpdate } from "../../contexts/postContext";
+import { UserContext } from "../../contexts/userContext";
 
 const Post = () => {
   const setPost = useContext(PostContextUpdate);
+  const user = useContext(UserContext);
+  const {
+    loading,
+    error,
+    response: loadedPosts,
+  } = PullData(`/post/${user?.user_id}`);
 
-  const { loading, error, response: loadedPosts } = useApiCall("get", "/post");
   useEffect(() => {
     setPost(loadedPosts);
   }, [loadedPosts]);
 
   if (loading) return <div>loading</div>;
-  if (error) return <div>error</div>;
+  if (!error) return <div>error</div>;
   return <ListPosts />;
 };
 export default Post;
