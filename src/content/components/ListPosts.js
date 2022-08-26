e2d024ad1d255549faf9193d51795a67b7630f66
kielon <mosadowski23@gmail.com>
2022-08-26 07:15:41 +0000

    user can now add and remove reactions to post

--- src/content/components/ListPosts.js
+++ src/content/components/ListPosts.js
@@ -1,32 +1,46 @@
 import React, { useContext, useEffect } from "react";
 
+import { Reactions } from "./Reactions";
+
 import { PostContext, PostContextUpdate } from "../../contexts/postContext";
 import { apiRequest } from "../../api/api";
 import { HTTP_METHODS } from "../../constants/httpMethods";
+import { UserContext } from "../../contexts/userContext";
+
 const ListPosts = () => {
   const post = useContext(PostContext);
   const setPost = useContext(PostContextUpdate);
+  const user = useContext(UserContext);
+
   const pullData = async () => {
-    const data = await apiRequest(
-      HTTP_METHODS.GET,
-      "/post/6ab0ce9d-28b2-483d-a1ec-cf1ec38db784"
-    );
+    const data = await apiRequest(HTTP_METHODS.GET, `/post/${user?.user_id}`);
     setPost(data);
   };
   useEffect(() => {
     pullData();
-  }, []);
+  }, [user]);
 
-  const listPosts = post?.result?.map(({ title, content, image, post_id }) => {
-    if (post.length === 0) return <div>loading</div>;
-    return (
-      <div key={post_id} className="content-post">
-        <h1 className="content-post-title">{title}</h1>
-        <div className="content-post-description">{content}</div>
-        <img src={image} alt="" className="message-image" />
-      </div>
-    );
-  });
+  const listPosts = post?.result?.map(
+    ({ title, content, image, post_id, likes, reactedValue }, i) => {
+      if (post.length === 0) return <div>loading</div>;
+      return (
+        <div key={i} className="content-post">
+          <div className="content-post-leftSide">
+            <Reactions
+              post_id={post_id}
+              likes={likes}
+              reactedValue={reactedValue}
+            />
+          </div>
+          <div className="content-post-rightSide">
+            <h1 className="content-post-title">{title}</h1>
+            <div className="content-post-description">{content}</div>
+            <img src={image} alt="" className="message-image" />
+          </div>
+        </div>
+      );
+    }
+  );
   return <div>{listPosts}</div>;
 };
 
---
+++ src/content/components/Reactions.js
@@ -0,0 +1,43 @@
+import { useContext } from "react";
+import { UserContext } from "../../contexts/userContext";
+import { PostContextUpdate } from "../../contexts/postContext";
+
+import beerFilledLike from "../../assets/beer-filled-like.png";
+import beerNotFilledLike from "../../assets/beer-not-filled-like.png";
+import beerFilledDislike from "../../assets/beer-filled-dislike.png";
+import beerNotFilledDislike from "../../assets/beer-not-filled-dislike.png";
+import { apiRequest } from "../../api/api";
+
+export const Reactions = ({ likes, reactedValue, post_id }) => {
+  const setPost = useContext(PostContextUpdate);
+  const user = useContext(UserContext);
+
+  const HandleClick = async (value) => {
+    await apiRequest("post", "/reaction/add", {
+      user_id: user.user_id,
+      post_id: post_id,
+      value: value,
+    });
+    const getResponse = await apiRequest("get", `/post/${user?.user_id}`);
+    setPost(getResponse);
+  };
+
+  return (
+    <div className="content-icons">
+      <img
+        onClick={(e) => HandleClick(1)}
+        className="content-icon-like"
+        alt=""
+        src={reactedValue === 1 ? beerFilledLike : beerNotFilledLike}
+      />
+
+      <div className="content-score">{likes}</div>
+      <img
+        onClick={(e) => HandleClick(-1)}
+        className="content-icon-dislike"
+        alt=""
+        src={reactedValue === -1 ? beerFilledDislike : beerNotFilledDislike}
+      />
+    </div>
+  );
+};
